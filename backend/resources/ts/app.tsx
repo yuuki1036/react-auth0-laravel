4e5d97549467443f8e0afa35208a378d312eceae
yuuki1036 <yuuki1036@gmail.com>
2022-02-06 14:54:17 +0900

    remove config

--- backend/resources/ts/app.tsx
+++ backend/resources/ts/app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Index from "./Pages/Index";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { REACT_APP_AUTH0_CLIENT_ID, REACT_APP_AUTH0_DOMAIN } from "./config";
 import { ThemeProvider } from "@mui/system";
 import MyTheme from "./theme";
 import { CssBaseline } from "@mui/material";
